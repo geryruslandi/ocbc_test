dad1284cad4882b7f54f250485d54f9f1f07999e
Gery Ruslandi <geryruslandi@gmail.com>
2022-03-08 10:46:53 +0700

    libraries requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './pages/login';
 
-const App = () => {
+const Root = createStackNavigator()
+
+
+export default function App(){
 
   return (
     <NavigationContainer>
-      {/* Rest of your app code */}
+      <Root.Navigator>
+          <Root.Screen options={{headerShown:false}} name="Login" component={Login}/>
+      </Root.Navigator>
     </NavigationContainer>
   );
 };
-
-
-export default App;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,40 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     return (
-        <View>
-            <Text>Test</Text>
+        <View style={{paddingTop: 60, paddingVertical: 30}}>
+            <View style={styles.logoContainer}>
+                <Image source={require('../../assets/images/ocbc.png')} style={styles.logoIcon}/>
+                <Text style={styles.logoLabel}>OCBC Bank</Text>
+            </View>
+            <View>
+
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 50
+    },
+    logoLabel: {
+        color: '#EF393B',
+        fontSize: 45,
+        fontFamily: 'OpenSans-ExtraBold',
+        letterSpacing: 0.5
+    },
+    logoIcon: {
+        height:50,
+        width: 50,
+        marginRight: 10,
+    }
+})
