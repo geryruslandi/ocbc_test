092547a89b9b819cce2110e6d5ed746f105f3cd0
Gery Ruslandi <geryruslandi@gmail.com>
2022-03-08 18:36:43 +0700

    persisted user login session finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { PersistGate } from 'redux-persist/integration/react';
 import Login from './pages/login';
 import { Provider } from 'react-redux';
-import AppStore from './store'
+import AppStore, { persistor } from './store';
 
 const Root = createStackNavigator()
-
 
 export default function App(){
 
   return (
     <Provider store={AppStore}>
-        <NavigationContainer>
-        <Root.Navigator>
-            <Root.Screen options={{headerShown:false}} name="Login" component={Login}/>
-        </Root.Navigator>
-        </NavigationContainer>
+        <PersistGate persistor={persistor}>
+            <NavigationContainer>
+            <Root.Navigator>
+                <Root.Screen options={{headerShown:false}} name="Login" component={Login}/>
+            </Root.Navigator>
+            </NavigationContainer>
+        </PersistGate>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { loggedInUserSlice } from './login-data';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: AsyncStorage,
+}
+
+const rootReducer = combineReducers({
+    loggedInUser: loggedInUserSlice.reducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-    reducer: {
-        loggedInUser: loggedInUserSlice.reducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export default store;
