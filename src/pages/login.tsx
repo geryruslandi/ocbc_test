7ab600cf697d2b7703153da9c0d0ee2b8a5a94b7
Gery Ruslandi <geryruslandi@gmail.com>
2022-03-08 10:47:22 +0700

    login page without api interaction

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,34 +1,68 @@
 import React, { useState } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import { Dimensions } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     return (
-        <View style={{paddingTop: 60, paddingVertical: 30}}>
+        <View style={styles.pageContainer}>
             <View style={styles.logoContainer}>
                 <Image source={require('../../assets/images/ocbc.png')} style={styles.logoIcon}/>
                 <Text style={styles.logoLabel}>OCBC Bank</Text>
             </View>
-            <View>
+            <View style={styles.inputContainer}>
+                <View style={{marginBottom: 30}}>
+                    <TextInput
+                        label="Username"
+                        value={username}
+                        onChangeText={setUsername}
+                        selectionColor="#EF393B"
+                        theme={{colors: {primary: '#EF393B'}}}
+                        style={{...styles.input, marginBottom: 10}}/>
+                    <TextInput
+                        label="password"
+                        theme={{colors: {primary: '#EF393B'}}}
+                        value={password}
+                        secureTextEntry
+                        onChangeText={setPassword}
+                        style={styles.input}/>
+                </View>
+                <Button
+                    color="#EF393B"
+                    mode="contained"
+                    onPress={() => console.log('Pressed')}>
 
+                    <Icon name="account-arrow-right" size={21} color="white"/>
+                    <View style={{ width: 5, height: 1 }} />
+                    <Text style={{fontSize: 15}}>Login</Text>
+                </Button>
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    pageContainer: {
+        paddingHorizontal: windowWidth * 0.1,
+        flex: 1,
+        backgroundColor: 'white'
+    },
     logoContainer: {
-        display: 'flex',
+        flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: 50
     },
     logoLabel: {
         color: '#EF393B',
-        fontSize: 45,
+        fontSize: 40,
         fontFamily: 'OpenSans-ExtraBold',
         letterSpacing: 0.5
     },
@@ -36,5 +70,15 @@ const styles = StyleSheet.create({
         height:50,
         width: 50,
         marginRight: 10,
+    },
+    inputContainer: {
+        marginVertical: 80
+    },
+    input: {
+        shadowColor: 'black',
+        shadowOffset: {width: 3, height: 5},
+        shadowOpacity: 0.2,
+        elevation: 1,
+        backgroundColor: 'white',
     }
 })
